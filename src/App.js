2a749849e1f1e3dfb72ccae58c667279c168a793
Abhishek37k <iabhishek6kumar@gmail.com>
2025-03-26 23:15:31 +0530

    Modal added  for cart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import Header from "./components/layout/Header";
+import Cart from "./components/cart/Cart";
 import Meals from './components/meals/Meals'
 function App() {
   return (
     <div>
+          <Cart />
       <Header/>
       <main>
         <Meals />

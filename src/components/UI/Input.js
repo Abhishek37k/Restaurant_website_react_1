5af5a472ee3f58473a4633de5fe6acb28539e883
Abhishek37k <iabhishek6kumar@gmail.com>
2025-03-26 22:32:45 +0530

    Form added for items to add to cart

---
+++ src/components/UI/Input.js
@@ -0,0 +1,11 @@
+const Input = (props) => {
+  return (
+    <>
+      <div>
+        <label>{props.label}</label>
+        <input  {...props.input} />
+      </div>
+    </>
+  );
+};
+export default Input;
